e2fc78fa8c22b70c9bdc84aaf857c3c06c7c8dc6
Vaisak29 <95977870+Vaisak29@users.noreply.github.com>
2021-12-11 23:24:29 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,228 @@
+const quizContainer = document.getElementById('quiz');
+console.log(quizContainer)
+const submitButton = document.getElementById('submit');
+const resultsContainer = document.getElementById('results');
+const myQuestions = [
+        {
+            question: "What does the passage describe?",
+            answers: {
+                a: "Arizona",
+                b: "Arizona State Flag",
+                c: "Stars and Stripes"
+            },
+
+            correctAnswer: "b"
+        },
+
+        {
+            question: "What is NOT on the flag?",
+            answers: {
+                a: "Copper",
+                b: "Star",
+                c: "Yellow and Red rays"
+            },
+
+            correctAnswer: "a"
+        },
+
+        {
+            question: "Where is the Star on the Flag?",
+            answers: {
+                a: "in the upper half",
+                b: "middle",
+                c: "lower half"
+            },
+
+            correctAnswer: "b"
+        },
+        {
+            question: "What is the purpose of having the star on the flag?",
+            answers: {
+                a: "symbolizes Arizona's many starsango",
+                b: "symbolic of the rising stararrot",
+                c: "symbolic of Arizona's copper industrypple",
+                d: "all of the above"
+            },
+
+            correctAnswer: "c"
+        },
+        {
+            question: "How many rays are on the flag?",
+            answers: {
+                a: "12",
+                b: "13",
+                c: "6",
+                d: "7"
+            },
+
+            correctAnswer: "b"
+        },
+        {
+            question: "What is the color blue on the Arizona flag compared to?",
+            answers: {
+                a: "American Flag",
+                b: "Stars",
+                c: "Stripes"
+
+            },
+
+            correctAnswer: "a"
+        },
+
+        {
+            question: "Why are there red and yellow rays on the Arizona Flag?",
+            answers: {
+                a: "to make it pretty",
+                b: "to show a setting sun",
+                c: "to emphasize the star"
+            },
+
+            correctAnswer: "b"
+        },
+        {
+            question: "When did the flag become official?",
+            answers: {
+                a: "1917",
+                b: "1927",
+                c: "1937"
+            },
+
+            correctAnswer: "a"
+        },
+        
+    ];
+(function () {
+    // Functions
+    function buildQuiz() {
+        // variable to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach(
+            (currentQuestion, questionNumber) => {
+
+                // variable to store the list of possible answers
+                const answers = [];
+
+                // and for each available answer...
+                for (letter in currentQuestion.answers) {
+
+                    // ...add an HTML radio button
+                    answers.push(
+                        `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`);
+
+                }
+
+                // add this question and its answers to the output
+                output.push(
+                    `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`);
+
+            });
+
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults() {
+
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+
+
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} correct out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        }
+        else {
+            previousButton.style.display = 'inline-block';
+        }
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    // Variables
+    
+
+
+
+    // Kick things off
+    buildQuiz();
+
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    // Show the first slide
+    showSlide(currentSlide);
+
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+})();
+  var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
